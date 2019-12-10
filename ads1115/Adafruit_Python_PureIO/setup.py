b6ef9f1a0e8a05ef054a1fd273d7e0ff1c11c6d9
Oleg Milantiev <oleg@milantiev.com>
2019-12-10 21:11:27 +0000

    Добавил поддержку АЦП ads1115

---
+++ ads1115/Adafruit_Python_PureIO/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='Adafruit_PureIO',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description='Pure python (i.e. no native extensions) access to Linux IO including I2C and SPI.  Drop in replacement for smbus and spidev modules.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Adafruit Industries',
+    author_email='circuitpython@adafruit.com',
+    python_requires='>=3.4.0',
+    url='https://github.com/adafruit/Adafruit_Python_PureIO',
+
+    # If your package is a single module, use this instead of 'packages':
+    packages=['Adafruit_PureIO'],
+
+    install_requires=[],
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+    ],
+)
---
+++ ads1115/ads1115.todb.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import time
+import Adafruit_ADS1x15
+import mysql.connector
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+import config
+
+adc = Adafruit_ADS1x15.ADS1115()
+
+db = mysql.connector.connect(host=config.db['host'], user=config.db['user'], passwd=config.db['passwd'], database=config.db['database'], charset='utf8')
+cursor = db.cursor()
+
+GAIN = 1
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+
+minute = ""
+avg = [0] * 4
+
+for repeat in range(10):
+	for i in range(4):
+		avg[i] += adc.read_adc(i, gain=GAIN)
+
+#print(avg[0] / 10)
+#print(avg[1] / 10)
+
+ts = int(time.time())
+
+cursor.execute("""INSERT INTO sensor(date, channel, type, val)
+	VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+	"""%{"time":ts, "channel":0, "type": 'voltage', "val":avg[0] / 10 })
+
+cursor.execute("""INSERT INTO sensor(date, channel, type, val)
+	VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+	"""%{"time":ts, "channel":1, "type": 'voltage', "val":avg[1] / 10 })
+
+db.commit()
+
+cursor.close()
+db.close()
