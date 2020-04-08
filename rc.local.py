b8fec275b95801aaf1fef1e0760fb2c927160b6b
Oleg Milantiev <oleg@milantiev.com>
2020-04-08 14:44:23 +0300

    Add web server permission to control relays

--- rc.local.py
+++ rc.local.py
@@ -4,6 +4,11 @@ import config
 import mysql.connector
 import time
 
+import pwd
+import grp
+import os
+
+
 db = mysql.connector.connect(host=config.db['host'], user=config.db['user'], passwd=config.db['passwd'], database=config.db['database'], charset='utf8')
 cursor = db.cursor(dictionary=True)
 
@@ -18,6 +23,8 @@ for relay in config.relay:
 	f = open('/sys/class/gpio/gpio{}/direction'.format(relay['gpio']), 'wt')
 	f.write('out')
 	f.close()
+
+	os.chown('/sys/class/gpio/gpio{}/value'.format(relay['gpio']), pwd.getpwnam("www-data").pw_uid, grp.getgrnam("www-data").gr_gid)
 
 time.sleep(0.1)
 
