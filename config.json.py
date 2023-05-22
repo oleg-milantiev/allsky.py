852d01f6c29b8bae0dc5c1575557ef19fa756f17
Oleg Milantiev <oleg@milantiev.com>
2023-05-22 06:40:38 +0000

    Move config.py content to web

--- config.json.py
+++ config.json.py
@@ -5,16 +5,6 @@ import config
 
 with open('/opt/allsky.py/config.py.json', 'w') as f:
 	f.write(json.dumps({
-		'name':			config.name,
-		'ccd':			config.ccd,
-		'publish':		config.publish,
-		'processing':	config.processing,
-		'sensors':		config.sensors,
-		'keogram':		config.keogram,
-		'annotations':	config.annotations,
-		'archive':		config.archive,
-		'video':		config.video,
-		'relay':		config.relay,
 		'db':			config.db,
 		'path':			config.path,
 	}))
