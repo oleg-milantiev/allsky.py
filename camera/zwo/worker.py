3793227ccfeb57b1496f0ade78b32d93963c5820
Oleg Milantiev <oleg@milantiev.com>
2023-06-07 13:13:37 +0300

    Start to do ZWO

--- camera/zwo/worker.py
+++ camera/zwo/worker.py
@@ -2,17 +2,40 @@
 
 import os
 
-os.system('/common/udev.py')
+#os.system('/common/udev.py')
 
 import pika
 import time
 import json
 
-import qhyccd
+import zwoasi as asi
 from astropy.io import fits
 from datetime import datetime
 
-qc = qhyccd.qhyccd()
+asi.init('/camera/'+ os.getenv('ARCH') +'/libASICamera2.so')
+
+num_cameras = asi.get_num_cameras()
+if num_cameras == 0:
+    print('No cameras found')
+    sys.exit(0)
+
+cameras_found = asi.list_cameras()  # Models names of the connected cameras
+
+if num_cameras == 1:
+    print('Found one camera: %s' % cameras_found[0])
+else:
+    print('Found %d cameras' % num_cameras)
+    for n in range(num_cameras):
+        print('    %d: %s' % (n, cameras_found[n]))
+    print('Using #%d: %s' % (camera_id, cameras_found[0]))
+
+camera = asi.Camera(0)
+#camera_info = camera.get_camera_property()
+controls = camera.get_controls()
+camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, controls['BandWidth']['MinValue'])
+camera.disable_dark_subtract()
+camera.stop_video_capture()
+camera.stop_exposure()
 
 print(' [*] Start')
 
@@ -30,30 +53,30 @@ channel = connection.channel()
 channel.queue_declare(queue=os.getenv('RABBITMQ_QUEUE_CAMERA'), durable=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
-exposure = 100
+exposure = None
 gain = None
 
 def callback(ch, method, props, body):
-    global qc, gain, exposure
+    global camera, gain, exposure, cameras_found
 
     payload = json.loads(body.decode())
     print(" [x] Received %r" % body.decode())
 
     if payload['gain'] != gain:
         gain = payload['gain']
-        qc.SetGain(gain)
+        camera.set_control_value(asi.ASI_GAIN, gain)
 
     if payload['exposure'] != exposure:
         exposure = payload['exposure']
-        qc.SetExposure(round(exposure * 1000))
+        camera.set_control_value(asi.ASI_EXPOSURE, round(exposure * 1000))
 
     hdr = fits.Header()
     hdr['TELESCOP'] = 'AllSky'
-    hdr['INSTRUME'] = qc.id.value.decode("utf-8")
+    hdr['INSTRUME'] = cameras_found[0] #.decode("utf-8")
     hdr['GAIN'] = gain
     hdr['EXPTIME'] = exposure
     hdr['DATE-OBS'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    hdu = fits.PrimaryHDU(qc.GetSingleFrame(), header=hdr)
+    hdu = fits.PrimaryHDU(camera.capture(), header=hdr)
     hdul = fits.HDUList([hdu])
     hdul.writeto('/fits/current.fit', overwrite=True)
 
