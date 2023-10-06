646653652f3295422b6b8936401002363de84a54
Oleg Milantiev <oleg@milantiev.com>
2023-10-06 10:52:44 +0300

    Removed outdated

--- camera/common/config.py
+++ camera/common/config.py
@@ -5,27 +5,10 @@ db = {
 	'database':	'allsky',
 }
 
-path = {
-	'web':		'/opt/allsky.py/web/',
-	'ffmpeg':	'/usr/bin/ffmpeg',
-}
-
 import logging
 
 log = {
-#	'path':		'/var/log/allsky.log',	# путь до лога. И уровень болтливости (debug = максимальный)
 	'path':		'/dev/stdout',
-	'level':	logging.INFO #DEBUG,			# менее подробные: logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL
+	'level':	logging.INFO# 			# менее подробные: logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL
+#	'level':	logging.DEBUG,
 }
-
-
-### дальше не править
-
-if (path['web'][-1] != '/'):
-	path['web'] += '/'
-
-path['jpg'] = path['web'] +'snap/'
-path['keogram'] = path['web'] +'keogram/'
-path['video'] = path['web'] +'video/'
-path['video-demand'] = path['web'] +'video-demand/'
-path['fit'] = path['web'] + 'fits/'
