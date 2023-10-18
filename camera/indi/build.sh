9069e7ce3ac5859a4ec336ce1929ff0446a8ccad
Oleg Milantiev <oleg@milantiev.com>
2023-10-18 19:56:52 +0300

    INDI: move to ubuntu

--- camera/indi/build.sh
+++ camera/indi/build.sh
@@ -1,2 +1,2 @@
 #docker build --squash -t olegmilantiev/allsky-indi .
-docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-indi .
+docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-indi:ubuntu Dockerfile.ubuntu
