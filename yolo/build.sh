702dd75812d0d8c25ffc213eaba8700940c577c2
Oleg Milantiev <oleg@milantiev.com>
2023-10-28 18:56:47 +0300

    YOLO: Build CPU-only images (less size on x64)

--- yolo/build.sh
+++ yolo/build.sh
@@ -1,2 +1,2 @@
-docker build --squash -t olegmilantiev/allsky-yolo .
-#docker buildx build --push --platform linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-yolo .
+#docker build --squash -t olegmilantiev/allsky-yolo .
+docker buildx build --push --platform linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-yolo .
