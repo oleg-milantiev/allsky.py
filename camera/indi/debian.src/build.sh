1667dcb6d6446f28ffb5de9586099e5cba6373c5
Oleg Milantiev <oleg@milantiev.com>
2023-10-31 10:47:44 +0300

    Docker: INDI on arm64 work only with src build

--- camera/indi/debian.src/build.sh
+++ camera/indi/debian.src/build.sh
@@ -1,2 +1,2 @@
-#docker build --squash -t olegmilantiev/allsky-indi .
+#docker build --squash -t olegmilantiev/allsky-indi-arm64 .
 docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-indi .
