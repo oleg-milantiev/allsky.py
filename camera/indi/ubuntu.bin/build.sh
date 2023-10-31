30c789782bcb75645307cc1d983d62c410e36b86
Oleg Milantiev <oleg@milantiev.com>
2023-10-31 14:25:19 +0300

    Docker: Indi: rebuild from src

---
+++ camera/indi/ubuntu.bin/build.sh
@@ -0,0 +1,3 @@
+#docker build --squash -t olegmilantiev/allsky-indi .
+#docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-indi .
+docker buildx build --push --platform linux/amd64 --squash -t olegmilantiev/allsky-indi-x64 .
