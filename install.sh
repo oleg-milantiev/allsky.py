df2165e95b3205d1c5998f1314eca819a9a5b10b
Oleg Milantiev <oleg@milantiev.com>
2023-11-04 11:48:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ if dialog --stdout --title "Need to install latest DOCKER?" \
 	apt-get update
 	apt-get install -y ca-certificates curl gnupg
 	install -m 0755 -d /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	chmod a+r /etc/apt/keyrings/docker.gpg
 
 	echo \
