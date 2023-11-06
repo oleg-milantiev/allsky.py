981e57e79758e5cc1e7e931867dc0fe7243b6ef0
Oleg Milantiev <oleg@milantiev.com>
2023-11-07 01:45:00 +0300

    Sensors: BME280(*): Collector

--- sensor/worker.py
+++ sensor/worker.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
 
-#from astropy.io import fits
 from datetime import datetime#, timedelta
-#import json
 import logging
 import MySQLdb
 import os
-#import pika
-#import re
 import signal
 import sys
 
-# I2C BME280
-import smbus2
-import bme280
-import time
-#from threading import Thread
-
-# SPI BME280
+# https://docs.circuitpython.org/projects/bme280/en/latest/examples.html
 import board
-import busio
-import digitalio
-import adafruit_bme280
-
+from adafruit_bme280 import basic as adafruit_bme280
 
 sys.path.insert(0, '/camera')
 import config
@@ -35,8 +22,6 @@ def terminate(signal,frame):
 
 signal.signal(signal.SIGTERM, terminate)
 
-#web = lib.getWebConfig()
-
 logging.basicConfig(filename=config.log['path'], level=config.log['level'])
 
 logging.info('[+] Discovering sensors')
@@ -46,6 +31,7 @@ channels = []
 # try BME280 i2c 
 logging.info('[.] Searh BME280(i2c)')
 
+'''
 for f in os.listdir('/dev'):
 	if f[0:4] == 'i2c-':
 		port = int(f[4:])
@@ -61,18 +47,31 @@ for f in os.listdir('/dev'):
 			channels.append({'sensor': 'bme280', 'bus': 'i2c', 'port': port})
 		except:
 			logging.debug('[-] No BME280 on /dev/'+ f)
+'''
+try:
+	i2c = board.I2C()  # uses board.SCL and board.SDA
+	bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+	logging.info('[+] Found BME280(I2C)')
+	channels.append({'sensor': 'bme280', 'bus': 'i2c'})
+except:
+	logging.debug('[-] No BME280(I2C) is found')
 
 try:
-	spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-	cs = digitalio.DigitalInOut(board.D22)
-	data = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
+	spi = board.SPI()
+	bme_cs = digitalio.DigitalInOut(board.D10) # D22?
+	bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
+
+	#spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+	#cs = digitalio.DigitalInOut(board.D22)
+	#data = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
 
 	logging.info('[+] Found BME280(SPI)')
 	channels.append({'sensor': 'bme280', 'bus': 'spi'})
 except:
 	logging.debug('[-] No BME280 on SPI')
 
-#print(channels)
+print(channels)
 
 logging.info('[+] Start to serve')
 
@@ -81,60 +80,67 @@ minute = datetime.now().strftime("%Y-%m-%d_%H-%M")
 while True:
 
 	if len(channels) > 0:
+		db = MySQLdb.connect(host=config.db['host'], user=config.db['user'], passwd=config.db['passwd'],
+			 database=config.db['database'], charset='utf8')
+
+		cursor = db.cursor()
+
+		ts = int(time.time())
+
 		# get sensors data
 		for channel in range(len(channels)):
 			data = None
 
 			if channels[channel]['sensor'] == 'bme280':
 				if channels[channel]['bus'] == 'i2c':
-					address = 0x76
-					bus = smbus2.SMBus(channels[channel]['port'])
-					bme280.load_calibration_params(bus, address)
-					data = bme280.sample(bus, address)
+					i2c = board.I2C()  # uses board.SCL and board.SDA
+					data = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+					#address = 0x76
+					#bus = smbus2.SMBus(channels[channel]['port'])
+					#bme280.load_calibration_params(bus, address)
+					#data = bme280.sample(bus, address)
 
 				elif channels[channel]['bus'] == 'spi':
-					spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-					cs = digitalio.DigitalInOut(board.D22)
-					data = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
+					spi = board.SPI()
+					bme_cs = digitalio.DigitalInOut(board.D10) # D22?
+					bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
+
+					#spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+					#cs = digitalio.DigitalInOut(board.D22)
+					#data = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
 
 			if data is not None:
 				print(data)
 
-			# store sensors data
-			db = MySQLdb.connect(host=config.db['host'], user=config.db['user'], passwd=config.db['passwd'],
-				 database=config.db['database'], charset='utf8')
+				# store sensors data
+				cursor.execute("""INSERT INTO sensor(date, channel, type, val)
+					VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+					"""%{"time":ts, "channel":channel, "type": 'temperature', "val":data.temperature })
 
-			cursor = db.cursor()
+				cursor.execute("""INSERT INTO sensor(date, channel, type, val)
+					VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+					"""%{"time":ts, "channel":channel, "type": 'humidity', "val":data.relative_humidity })
 
-			ts = int(time.time())
-
-			cursor.execute("""INSERT INTO sensor(date, channel, type, val)
-				VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
-				"""%{"time":ts, "channel":channel, "type": 'temperature', "val":data.temperature })
-
-			cursor.execute("""INSERT INTO sensor(date, channel, type, val)
-				VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
-				"""%{"time":ts, "channel":channel, "type": 'humidity', "val":data.humidity })
-
-			cursor.execute("""INSERT INTO sensor(date, channel, type, val)
-				VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
-				"""%{"time":ts, "channel":channel, "type": 'pressure', "val":data.pressure * 0.75 })
+				cursor.execute("""INSERT INTO sensor(date, channel, type, val)
+					VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+					"""%{"time":ts, "channel":channel, "type": 'pressure', "val":data.pressure * 0.75 })
 
 
-			cursor.execute("""REPLACE INTO sensor_last(date, channel, type, val)
-				VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
-				"""%{"time":ts, "channel":channel, "type": 'temperature', "val":data.temperature })
+				cursor.execute("""REPLACE INTO sensor_last(date, channel, type, val)
+					VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+					"""%{"time":ts, "channel":channel, "type": 'temperature', "val":data.temperature })
 
-			cursor.execute("""REPLACE INTO sensor_last(date, channel, type, val)
-				VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
-				"""%{"time":ts, "channel":channel, "type": 'humidity', "val":data.humidity })
+				cursor.execute("""REPLACE INTO sensor_last(date, channel, type, val)
+					VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+					"""%{"time":ts, "channel":channel, "type": 'humidity', "val":data.humidity })
 
-			cursor.execute("""REPLACE INTO sensor_last(date, channel, type, val)
-				VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
-				"""%{"time":ts, "channel":channel, "type": 'pressure', "val":data.pressure * 0.75 })
+				cursor.execute("""REPLACE INTO sensor_last(date, channel, type, val)
+					VALUES (%(time)i, %(channel)i, '%(type)s', %(val)f)
+					"""%{"time":ts, "channel":channel, "type": 'pressure', "val":data.pressure * 0.75 })
 
-			cursor.close()
-			db.close()
+		cursor.close()
+		db.close()
 
 	logging.debug('[.] Waiting for next minute')
 	while True:
