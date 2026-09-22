3525924e91e90fbbc6976256eb6dee261de083d4
Oleg Milantiev <Oleg.Milantiev@okkam.ru>
2026-09-22 17:31:29 +0300

    Rebuild (compact) docker image

--- camera/indi/debian.src/build.sh
+++ camera/indi/debian.src/build.sh
@@ -1,4 +1,4 @@
 #docker build --squash -t olegmilantiev/allsky-indi-arm64 .
 
-#docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-indi .
-docker buildx build --push --platform linux/arm64,linux/amd64 --squash -t olegmilantiev/allsky-indi .
+docker buildx build --push --platform linux/arm64,linux/amd64 -t olegmilantiev/allsky-indi .
+#docker buildx build --platform linux/amd64 -t olegmilantiev/allsky-indi .
